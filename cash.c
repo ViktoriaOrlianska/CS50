be19ba8dc711aa6ca0dae88cc4e245b94414d824
ViktoriaOrlianska <105880312+ViktoriaOrlianska@users.noreply.github.com>
2022-05-28 22:14:49 +0300

    Update cash.c

--- cash.c
+++ cash.c
@@ -7,60 +7,39 @@ int calculate_dimes(int cents);
 int calculate_nickels(int cents);
 int calculate_pennies(int cents);
 
+void cash(void);
+
+int get_cents (void){
+    int cents;
+    
+    do
+    {
+        cents = get_int("Change owed: ");
+    }
+    while (cents < 1 || cents > 99);
+    
+    return cents;
+}
+
+int calculate_quarters (int cents){
+     return (cents/25);
+}
+
+int calculate_dimes (int cents){
+     return (cents%25/10);
+}
+
+int calculate_nickels (int cents){
+     return (cents%25%10/5);
+}
+
+int calculate_pennies (int cents){
+     return (cents%25%10%5);
+}
+
 int main(void)
 {
-    // Ask how many cents the customer is owed
-    int cents = get_cents();
-
-    // Calculate the number of quarters to give the customer
-    int quarters = calculate_quarters(cents);
-    cents = cents - quarters * 25;
-
-    // Calculate the number of dimes to give the customer
-    int dimes = calculate_dimes(cents);
-    cents = cents - dimes * 10;
-
-    // Calculate the number of nickels to give the customer
-    int nickels = calculate_nickels(cents);
-    cents = cents - nickels * 5;
-
-    // Calculate the number of pennies to give the customer
-    int pennies = calculate_pennies(cents);
-    cents = cents - pennies * 1;
-
-    // Sum coins
-    int coins = quarters + dimes + nickels + pennies;
-
-    // Print total number of coins to give the customer
-    printf("%i\n", coins);
-}
-
-int get_cents(void)
-{
-    // TODO
-    return 0;
-}
-
-int calculate_quarters(int cents)
-{
-    // TODO
-    return 0;
-}
-
-int calculate_dimes(int cents)
-{
-    // TODO
-    return 0;
-}
-
-int calculate_nickels(int cents)
-{
-    // TODO
-    return 0;
-}
-
-int calculate_pennies(int cents)
-{
-    // TODO
-    return 0;
+    int cash = get_cents();
+    printf("%d\n",calculate_quarters(cash)+calculate_dimes(cash)+calculate_nickels(cash)+calculate_pennies(cash));
+    
 }
